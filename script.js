bcaa969b9cd85fa8ffdc59bfc145f7ce14766cb7
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-06-17 13:26:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
+// Your code here.
+
 const lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
+  A: "N",
+  B: "O",
+  C: "P",
+  D: "Q",
+  E: "R",
+  F: "S",
+  G: "T",
+  H: "U",
+  I: "V",
+  J: "W",
+  K: "X",
+  L: "Y",
+  M: "Z",
+  N: "A",
+  O: "B",
+  P: "C",
+  Q: "D",
+  R: "E",
+  S: "F",
+  T: "G",
+  U: "H",
+  V: "I",
+  W: "J",
+  X: "K",
+  Y: "L",
+  Z: "M",
+  "?": "?",
+  ",": ",",
 };
 
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
+function rot13(encodedStr) {
+  const words = encodedStr.split(" "); // String to Array
+  let decodedArr = []; // Your Result goes here
   // Only change code below this line
-	for(let i = 0 ; i < encodedStr.length ; i++) {
-        if(lookup[ encodedStr[i] ] === undefined){
-            decoderArr.push( encodedStr[i] ) ;
-        }   
-        else{ 
-            decoderArr.push(lookup[ encodedStr[i] ]);
-        }
-
-  return decodedArr;//return decodedArr
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    let decoded_word = "";
+    for (let j = 0; j < word.length; j++) {
+      const char = word.charAt(j);
+      const decoded_char = lookup[char];
+      decoded_word += decoded_char;
+    }
+    decodedArr.push(decoded_word);
+  }
+  return decodedArr.join(" ");
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(rot13("SERR YBIR? NPPVBWBO"));
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
 
-module.exports = rot13;
+// module.exports = rot13;
+window.rot13 = rot13;
